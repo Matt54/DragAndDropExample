179768dbdbff52e2d270fae8a8cb60175d0cc043
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2021-03-17 20:08:10 -0500

    Initial Commit

---
+++ DragAndDropExample/DragAndDropExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropExampleApp.swift
+//  DragAndDropExample
+//
+//  Created by Macbook on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
